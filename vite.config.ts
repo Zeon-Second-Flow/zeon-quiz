0c4a9d79959b43997531e84cfcba2c7e39416205
theoddorrrrr <aidar140702@mail.ru>
2022-10-20 19:12:54 +0600

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,38 @@ import path from "node:path";
 
 /* eslint-disable  import/no-default-export */
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), svgr()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src")
+export default defineConfig( ({command}) => {
+
+    const config =  {
+        // plugins: [isDevEnv && react(), svgr()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src")
+            }
+        },
+        server: {
+            port: 3000,
+            // https: {
+            //     key: fs.readFileSync("key.pem"),
+            //     cert: fs.readFileSync("cert.pem"),
+            // }
         }
-    },
-    server: {
-        port: 3000,
-        // https: {
-        //     key: fs.readFileSync("key.pem"),
-        //     cert: fs.readFileSync("cert.pem"),
-        // }
+    };
+    
+    if (command === "build"){
+        return {
+            ...config,
+            esbuild: {
+                jsxInject: "import React from \"react\";",
+            }
+        };
     }
+    
+    return {
+        ...config,
+        plugins: [react(), svgr()]
+    };
+    
+
 });
 /* eslint-enable  import/no-default-export */
