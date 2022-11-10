30d35c7e8286215664a040293a8726e5b2d3ff5a
terracotta <begaiym.akylova@gmail.com>
2022-11-10 15:12:26 +0600

    Change websocket path

--- src/store/websocket/websocket.ts
+++ src/store/websocket/websocket.ts
@@ -2,8 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 import { io } from 'socket.io-client';
 
 const initialState = {
-	// socket: io("https://obscure-eyrie-08815.herokuapp.com/", {
-	socket: io('http://localhost:3333', {
+	socket: io('https://lit-sea-75298.herokuapp.com/', {
+		// socket: io('http://localhost:3333', {
 		transports: ['websocket', 'polling'],
 	}),
 	room: 0,
